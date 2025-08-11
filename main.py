fd8da9b220fde1f327cadc8a61c89dded10d395f
FERNANDO JOSE CASTILLO TICONA <i2512095@continental.edu.pe>
2025-08-10 23:50:42 -0500

    corrigiendo error sintax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from menu import mostrar_menu
 
-st.set_page_congig(page_title="Simulador de creditos", layout ="wide",
-mostrar_menu )
+st.set_page_config(page_title="Simulador Bancario", layout="wide")
+mostrar_menu()
